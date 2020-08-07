0874b8237dd190631ae11d3056faf80635eaaaca
akumar1 <akumar1@empoweredbenefits.com>
2020-08-07 21:02:53 +0530

    added img url

--- components/card/card.js
+++ components/card/card.js
@@ -35,8 +35,8 @@ const Card = (props) => {
       <div className={styles.card}>
         <div className={styles.imgContainer}>
           <img
-            //   src={props.links.mission_patch}
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+            src={props.links.mission_patch}
+            // src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
             alt="Avatar"
             className={styles.image}
           />
